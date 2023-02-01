f6efba36c9fa466bd16bd576ede22456af5a280a
lishijm <lishijm@outlook.com>
2023-02-01 16:39:56 +0800

    day2

---
+++ 3_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    float a=3.74;
+    int b=-10;
+    unsigned int d=2;
+    char c='x';
+    if((b+d)>0){
+        printf("d\n");
+    }
+    printf("%u\n",(b+d));
+    //b=a;
+    //printf("%d",a);
+
+    return 0;
+}
