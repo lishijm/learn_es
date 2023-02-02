4734cf7857bf7e4ff6a5be9f109ca021d64d9536
lishijm <lishijm@outlook.com>
2023-02-02 10:42:04 +0800

    04

--- 4_2.c
+++ 4_2.c
@@ -6,7 +6,7 @@ int main(){
     printf("%d %d %d\n",a,b,c);*/
 
     int a=3,b=4,c=5,d=0;
-    d=a>b?b++:c++;
+    d=a>b?b++:c++;//三目运算符，若关系式1成立则该值为关系式2，若关系式1不成立则值为关系式3
     printf("%d %d %d %d",a,b,c,d);
 
     return 0;
