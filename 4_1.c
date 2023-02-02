2b0b42c0130b686646d39701ecedc801bb72d935
lishijm <lishijm@outlook.com>
2023-02-02 09:24:32 +0800

    04

--- 4_1.c
+++ 4_1.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a=1;
-    printf("%d\n",a++);//先赋值再运算
-    printf("%d\n",++a);//先运算再赋值
-
+    int a=-1;
+    if(0<a<5){
+        printf("d");
+    }
+    /*printf("%d\n",a++);//先赋值再运算
+    printf("%d\n",++a);//先运算再赋值*/
     return 0;
 }
