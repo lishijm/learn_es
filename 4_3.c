b374ec458f6d219a25832f13a0cd4411e7c4a3ca
lishijm <lishijm@outlook.com>
2023-02-02 15:07:24 +0800

    04

---
+++ 4_3.c
@@ -0,0 +1,22 @@
+//00010011
+#include<stdio.h>
+int main(){
+    //int a[9],b[9],c[9];
+    int i=11010011;
+    /*for(i=0;i<9;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<9;i++){
+        scanf("%d",&b[9]);
+    }
+    for(i=0;i<9;i++){
+        c[i]=a[i]&b[i];
+    }
+    for(i=0;i<9;i++){
+        printf("%d",c[9]);
+    }*/
+    
+    printf("%d",i&11111000);
+
+    return 0;
+}
---
+++ 4_4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=-4;
+
+    printf("%d\n",a>>2);
+    printf("%d\n",a<<2);
+
+    return 0;
+}
