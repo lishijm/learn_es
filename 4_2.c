4c54f8d5f5f4647ca5013fc2aff21d6e8cda3655
lishijm <lishijm@outlook.com>
2023-02-02 10:37:19 +0800

    04

---
+++ 4_2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    //int a=0,b=0,c=0,d=0;
+    /*c=(a-=a-5);
+    (a=b,b+=4);
+    printf("%d %d %d\n",a,b,c);*/
+
+    int a=3,b=4,c=5,d=0;
+    d=a>b?b++:c++;
+    printf("%d %d %d %d",a,b,c,d);
+
+    return 0;
+}
