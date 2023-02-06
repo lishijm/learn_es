4dd8838930d54f652dc72e1f1b5177dd82ff0c76
lishijm <lishijm@outlook.com>
2023-02-06 11:11:46 +0800

    06

--- c_prime/6_1.c
+++ c_prime/6_1.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main(){
-    int *p,i,**pp;
-    int num[]={1,2,3,4};
+    int *p,i,**pp,*o;
+    auto int num[]={1,2,3,4};
     p=num;
     pp=&p;
 
     for(i=0;i<4;i++,p++){
         printf("%d\n",*p);
         printf("%p\n",&num[i]);
+        printf("%p\n",p);
         printf("%p\n",&p);
+        printf("%p\n",&pp);
+        printf("%p\n",&o);
+        printf("%p\n",&i);
         printf("%d\n",**pp);
         printf("\n");
     }
