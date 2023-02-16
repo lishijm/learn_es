53c6552291071d895feb4dd588699e8de17e993e
lishijm <lishijm@outlook.com>
2023-02-16 13:31:20 +0800

    13

---
+++ shell/3-4.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+read a
+case $a in
+	1)
+		echo 1
+		;;
+	2)
+		echo 2
+		;;
+	3)
+		echo 3
+		;;
+	*)
+		echo '*'
+		;;
+esac
---
+++ shell/3-5.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+for i in {1,15,6,8,6,775,6}
+do
+	echo $i
+done
