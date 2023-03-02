8056f2c151f7183ab5550effadd6934f97ddcbb1
lishijm <lishijm@outlook.com>
2023-03-02 21:02:47 +0800

    24

--- data_str/4-6.c
+++ data_str/4-6.c
@@ -1,3 +1,4 @@
+//有问题
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
--- data_str/4-9.c
+++ data_str/4-9.c
@@ -2,6 +2,23 @@
 #include<stdlib.h>
 #include<string.h>
 
+typedef int datat;   //int -->  datat
+
+typedef struct arrs{
+	datat data[5];
+	int count;
+} ars;
+
+ars *crqe(void){
+	ars *pq = NULL;
+	pq = (ars *)malloc(sizeof(ars));
+	memset(pq, 0, sizeof(ars));
+	return pq;
+}
+
 int main(){
+    ars *p=NULL;
+    p=(ars *)malloc(sizeof(ars));
     
+    return 0;
 }
