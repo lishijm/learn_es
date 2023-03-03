89e12ed9aa48f8c5709549193b6d155c7946fcc7
lishijm <lishijm@outlook.com>
2023-03-03 13:17:31 +0800

    25

--- data_str/4-5.c
+++ data_str/4-5.c
@@ -17,6 +17,13 @@ typedef struct link{
     struct link *next;
 } link;
 
+stu *creainf(void){
+    stu *ninf=NULL;
+    ninf=(stu *)malloc(sizeof(stu));
+    memset(ninf,0,sizeof(stu));
+    return ninf;
+}
+
 link *creanode(void){
     link *plink=NULL;
     plink=(link *)malloc(sizeof(link));
@@ -24,60 +31,88 @@ link *creanode(void){
     return plink;
 }
 
-void makelink(link *phead1,datat ele){
-    link *ptmp1=NULL;
-    ptmp1=crlink();
-    
-    phead1->next=ptmp1;
-    ptmp1->data=ele;
-    int i;
-    for(i=1;i<100;i++){
-        link *a=NULL;
-        a=crlink();
-        a->data=i;
-        ptmp1->next=a;
-        ptmp1=ptmp1->next;
-    }
+stu *makeinf(void){
+    stu *newinf=NULL;
+    newinf=creainf();
+    scanf("%d",&newinf->data);
+    scanf("%s",newinf->name);
+
+    return newinf;
 }
 
-void output(link *p){
-    link *ptmp=NULL;
-    ptmp=crlink();
-    ptmp=p;
-    while(ptmp->next!=NULL){
-        ptmp=ptmp->next;
-        printf("%d ",ptmp->data);
+link *makenode(){
+    link *newnode=NULL;
+    newnode=creanode();
+    stu *newinf=NULL;
+    newinf=makeinf();
+    newnode->inf=newinf;
+
+    return newnode;
+}
+
+link* makehashlink(link *block[],link *new,int legnth){
+    link *tmp;
+    int in;
+    in=((new->inf->data)%legnth)-1;
+    tmp=block[in];
+    new->next=tmp->next;
+    tmp->next=new;
+}
+
+void serach(link *block[],datat fele,int legnth){
+    int i=0,flag=1;
+    link *tmp;
+    tmp=creanode();
+    for (i=0;i<legnth;i++){
+        tmp=block[i];
+        while(tmp->next!=NULL){
+            if(tmp->next->inf->data==fele){
+                printf("at NO.%d link\n",i+1);
+                flag=0;
+                break;
+            }
+            tmp=tmp->next;
+        }
+        if(0==flag){
+            break;
+        }
     }
-    printf("\n");
+    if(1==flag){
+        printf("not exist\n");
+    }
+    
 }
 
 int main(){
 
-    int i;
-    link *block[13];
-    for(i=0,i<13;i++){
+    int i,legnth,in;
+    datat fele;
+    scanf("%d",&legnth);
+    link *block[legnth];
+
+    for(i=0;i<legnth;i++){
         block[i]=NULL;
+        block[i]=creanode();
     }
     int sel;
     while(flag){    
         scanf("%d",&sel);
         switch (sel){
         case 1:
-            //
+            link *new=NULL;
+            new=creanode();
+            new=makenode();
+            makehashlink(block,new,legnth);
             break;
-        
+        case 2:
+            scanf("%d",&fele);
+            serach(block,fele,legnth);
+            break;
         default:
             flag=0;
             break;
         }
     }
-    makelink(phead);
-    output(phead);
-    int n;
-    scanf("%d",&n);
-    
-    phead=back(phead,n);
-    output(phead);
 
     return 0;
 }
