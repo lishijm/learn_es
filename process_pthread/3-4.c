075bffc40d70d91cc665bf100a7b965ebf2cf068
lishijm <lishijm@outlook.com>
2023-03-10 15:38:33 +0800

    30

---
+++ process_pthread/3-4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<string.h>
+#include <fcntl.h>
+
+int main(){
+    printf("main\n");
+    alarm(2);
+    pause();
+    printf("hello,world\n");
+    return 0;
+}
